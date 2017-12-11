c19a822c9347908e5c99de8976c5ffa8c91a5b78
SAMSUNG-NB\SAMSUNG <songsawas_bunsongkij@cmu.ac.th>
2017-12-12 00:01:45 +0700

    ต่าง ๆ

--- ng-app/src/app/user/user-delete/user-delete.component.ts
+++ ng-app/src/app/user/user-delete/user-delete.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductDataService} from "../../service/product-data-service";
+import {Product} from "../product";
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-user-delete',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDeleteComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productDataService: ProductDataService, private router: Router) {
+  }
+
+  products: Product[];
+
 
   ngOnInit() {
+    this.productDataService.getProductData()
+      .subscribe(products => this.products = products);
+
+
+  }
+
+  showDetail(product: Product) {
+    this.router.navigate(['/detail', product.productId]);
+
+  }
+
+
+
+  addToCart(product: Product) {
+    // var same = false;
+    // for(var i=0;i++;i<this.productDataService.productInCart.length) {
+    //   var string1 = this.productDataService.productInCart[i].productId;
+    //   var string2 = product.productId;
+    //   if (string1 == string2) {
+    //     same = true ;
+    //   }
+    // }
+    // if(same == false){
+
+      this.productDataService.getProductIncart().push(product);
+    //}
+  }
+
+  search: string;
+
+  onSearch() {
+    this.productDataService.findProduct(this.search).subscribe(products => this.products = products, (error) => {
+      if (error.status === 401) {
+        this.router.navigate(['prosuct'], {queryParams: {source: 'product'}})
+      }
+    })
   }
 
 }
