690e0f28c30bce5aef8efda1eb59d0b2f2dd03d2
SAMSUNG-NB\SAMSUNG <songsawas_bunsongkij@cmu.ac.th>
2017-12-12 07:52:28 +0700

    ทำ tran แก้ login

--- ng-app/src/app/transection/transection/transaction.component.ts
+++ ng-app/src/app/transection/transection/transaction.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductDataService} from "../../service/product-data-service";
+import {Product} from "../product";
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-transection',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private productDataService: ProductDataService, private router: Router) {
+  }
+
+
+
+  products: Product[];
+
 
   ngOnInit() {
+    this.productDataService.getProductData()
+      .subscribe(products => this.products = products);
+
+
   }
+
+  showDetail(product: Product) {
+    this.router.navigate(['/detail', product.productId]);
+
+  }
+
+
+
+  addToCart(product: Product) {
+      this.productDataService.getProductIncart().push(product);
+    //}
+  }
+
+  search: string;
+
+  onSearch() {
+    this.productDataService.findProduct(this.search).subscribe(products => this.products = products, (error) => {
+      if (error.status === 401) {
+        this.router.navigate(['product'], {queryParams: {source: 'product'}})
+      }
+    })
+  }
+
 
 }
